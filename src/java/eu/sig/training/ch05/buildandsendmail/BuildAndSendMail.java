54242c3306db19fda7151c99b09fea33f98ee37d
Jannik Boll Matthiassen <jannik@boll-nielsen.dk>
2021-06-09 13:57:52 +0200

    Refactored buildAndSendMail to have less input parameters

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -2,19 +2,12 @@ package eu.sig.training.ch05.buildandsendmail;
 
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
-        // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
-        // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
-        // Send message
-        m.send(mId, subject, mMessage);
+    public void buildAndSendMail(MailMan m, MailAccount account, Mail mail) {
+        String mId = account.getMailID();
+        m.send(mId, mail);
     }
     // end::buildAndSendMail[]
+
 
     @SuppressWarnings("unused")
     private MailMessage formatMessage(MailFont font, String string) {
@@ -24,7 +17,7 @@ public class BuildAndSendMail {
     private class MailMan {
 
         @SuppressWarnings("unused")
-        public void send(String mId, String subject, MailMessage mMessage) {}
+        public void send(String mId, Mail mail) {}
 
     }
 
@@ -32,8 +25,52 @@ public class BuildAndSendMail {
 
     }
 
+    private class Mail {
+        public Mail(String subject, MailMessage message) {
+        }
+    }
+
     private class MailMessage {
 
     }
 
+    public class Name {
+        private String firstName;
+        private String lastName;
+        public Name(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+    }
+
+    public class MailAccount{
+
+        private Name name;
+        private String division;
+        public MailAccount(Name name, String division) {
+            this.name = name;
+            this.division = division;
+        }
+
+        public Name getName() {
+            return name;
+        }
+
+        public String getDivision() {
+            return division;
+        }
+
+        public String getMailID() {
+            return name.getFirstName().charAt(0) + "." + name.getLastName().substring(0, 7) + "@"
+                    + division.substring(0, 5) + ".compa.ny";
+        }
+    }
 }
